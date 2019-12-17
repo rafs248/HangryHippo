0e76d554acadce756fcfb1ce357495494d487429
Rafal <rafal-wojciech.surowiec@digital.homeoffice.gov.uk>
2019-12-17 17:10:31 +0000

    Tests running

--- src/App.js
+++ src/App.js
@@ -2,9 +2,75 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  static defaultProps =
+      {
+        "id": 534573,
+        "title": "Brown Butter Apple Crumble",
+        "image": "https://spoonacular.com/recipeImages/534573-312x231.jpg",
+        "imageType": "jpg",
+        "usedIngredientCount": 1,
+        "missedIngredientCount": 2,
+        "missedIngredients": [
+          {
+            "id": 2010,
+            "amount": 0.5,
+            "unit": "tsp",
+            "unitLong": "teaspoons",
+            "unitShort": "tsp",
+            "aisle": "Spices and Seasonings",
+            "name": "cinnamon",
+            "original": "1/2 tsp cinnamon",
+            "originalString": "1/2 tsp cinnamon",
+            "originalName": "cinnamon",
+            "metaInformation": [],
+            "image": "https://spoonacular.com/cdn/ingredients_100x100/cinnamon.jpg"
+          },
+          {
+            "id": 8120,
+            "amount": 0.5,
+            "unit": "cup",
+            "unitLong": "cups",
+            "unitShort": "cup",
+            "aisle": "Cereal",
+            "name": "oats",
+            "original": "1/2 cup uncooked oats (not instant)",
+            "originalString": "1/2 cup uncooked oats (not instant)",
+            "originalName": "uncooked oats (not instant)",
+            "metaInformation": [
+              "uncooked",
+              "(not instant)"
+            ],
+            "image": "https://spoonacular.com/cdn/ingredients_100x100/rolled-oats.jpg"
+          }
+        ],
+        "usedIngredients": [
+          {
+            "id": 9003,
+            "amount": 4,
+            "unit": "",
+            "unitLong": "",
+            "unitShort": "",
+            "aisle": "Produce",
+            "name": "apples",
+            "original": "4 apples, peeled, cored and sliced",
+            "originalString": "4 apples, peeled, cored and sliced",
+            "originalName": "apples, peeled, cored and sliced",
+            "metaInformation": [
+              "cored",
+              "peeled",
+              "sliced"
+            ],
+            "image": "https://spoonacular.com/cdn/ingredients_100x100/apple.jpg"
+          }
+        ],
+        "unusedIngredients": [],
+        "likes": 7
+      };
+
   render() {
     return (
-      <div>
+      <div className="list-item-card">
         Hello World!
       </div>
     );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {shallow} from 'enzyme';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('renders a div with the className prop set to be an item card',()=>{
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('div').filterWhere(e => e.props().className === 'list-item-card').length).toBe(1);
+});
+
+it('renders stringified version of propps in the div',()=>{
+   const wrapper = shallow(<App/>);
+
+   expect(wrapper.find('div').findWhere(e=>e.props().className==='list-item-card').text())
+       .toBe(JSON.stringify(App.defaultProps))
 });
