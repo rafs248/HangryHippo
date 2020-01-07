bde2a0b3cd8ec86e4d0f097dfc1fbbdcf848eca0
Rafal <rafal-wojciech.surowiec@digital.homeoffice.gov.uk>
2020-01-07 15:38:11 +0000

    Fixed 3 unit tests

---
+++ src/ingredientsList/components/IngredientsList.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import IngredientListItem from "../../ingredientListItem/components/IngredientListItem";
+
+const propTypes = {
+    ingredients: PropTypes.array,
+    doSearch: PropTypes.func.isRequired,
+    canSearch: PropTypes.bool,
+};
+
+
+const IngredientsList = (props) => (
+    <div className="ingredients-panel">
+        <div className="ingredients-panel-text">
+            Quick give me a list of the ingredients in your fridge! Don't worry, we're here to help.
+        </div>
+        <div>
+            <button className="btn btn-info ingredients-go-button" onClick={props.doSearch}
+                    disabled={!props.canSearch}
+            >
+                Feed me !!!
+            </button>
+        </div>
+        <div className="ingredients-list-container">
+            {
+                props.ingredients.map((ingredient, index) => {
+                    return <IngredientListItem
+                        key={index}
+                        index={index}
+                        removeItem={props.removeIngredient}
+                    >{ingredient}
+                    </IngredientListItem>
+                    }
+                )
+            }
+        </div>
+    </div>
+);
+
+IngredientsList.propTypes = propTypes;
+
+export default IngredientsList;
